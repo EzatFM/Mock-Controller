10428107d5615f7a8d85c392eae7ecadb5466419
Ezat <1444564@cegepmontpetit.ca>
2024-11-22 10:52:25 -0500

    Auto stash before merge of "monLabo" and "origin/monLabo"

---
+++ mockTests/Controllers/CatsControllerTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mock.depart.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mock.depart.Controllers.Tests
+{
+    [TestClass()]
+    public class CatsControllerTests
+    {
+        [TestMethod()]
+        public void DeleteCatTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
